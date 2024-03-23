fd382e96b2636cdfe588e211ff85044b0ea6ed75
M ASRAN <62879334+theazran@users.noreply.github.com>
2024-03-23 00:05:17 +0000

    add Progressive Web App (PWA)

---
+++ public/serviceworker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "patrolwa-cache";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192x192.png",
+  "/icon-512x512.png",
+  "/serviceworker.js",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(() => {
+          return new Response("Anda sedang offline", {
+            status: 503,
+            statusText: "Offline",
+          });
+        })
+      );
+    }),
+  );
+});
