472902e01acc43566a04f2dd5501a80699e4fa69
M ASRAN <62879334+theazran@users.noreply.github.com>
2024-11-05 08:46:12 +0800

    update index

--- index.js
+++ index.js
@@ -65,6 +65,39 @@ fs.readFile("./package.json", "utf8", (err, data) => {
 
 const upload = multer({ storage: multer.memoryStorage() });
 
+const request = require("request");
+async function sendText(id, message) {
+  
+  var options = {
+    'method': 'POST',
+  'url': 'https://wa.panelpn.my.id/message/text?key=andalan',
+  'headers': {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer YOUR_TOKEN',
+    'Cookie': 'connect.sid=s%3AczNYsxJ8ssvtXkBvHrNoX1qTouJowdAX.jaL0VjfSkcVQhhJOGVBA7UJ7BPcbFQDKl979uTR9Xwc'
+  },
+  body: JSON.stringify({
+    "id": "6285255646434-1613359737@g.us",
+    "typeId": "group",
+    "message": "message",
+    "options": {
+      "delay": 0,
+      "replyFrom": ""
+    },
+    "groupOptions": {
+      "markUser": "ghostMention"
+    }
+  })
+  
+};
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
+});
+}
+sendText()
+
+
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/public");
 
